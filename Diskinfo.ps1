1f379c0fb21bcbf9cce0fc2fe32a28c370a7acf9
Minhas <Usman.Minhas@portofportland.com>
2019-10-31 14:40:06 -0700

    I don't like 49ers

---
+++ Diskinfo.ps1
@@ -0,0 +1,11 @@
+Param (
+   [Parameter(Mandatory=$True)]
+   [string]
+   $ServerName
+)
+
+
+Write-Host "Drive information for $ServerName"
+Get-WmiObject Win32_LogicalDisk -ComputerName $ServerName -Filter DriveType=3 | 
+Select-Object DeviceID, @{'Name'='Size (GB)'; 'Expression'={[string]::Format('{0:N0}',[math]::truncate($_.size / 1GB))}}, @{'Name'='Freespace (GB)'; 'Expression'={[string]::Format('{0:N0}',[math]::truncate($_.freespace / 1GB))}} | Out-Host
+
